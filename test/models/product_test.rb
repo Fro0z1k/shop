9db2d5fea060a034047176c45a4a5944fe19f4b0
Vitalik <vitalikstetsenko@hotmail.com>
2015-06-30 17:42:26 +0300

    add tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test 'product attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+  test 'product price must be positive' do
+    product = Product.new(title:        'Book Title',
+                          description:  'Book description',
+                          image_url:     'image.png')
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  # нет валидации на уникальность
+  test 'product is not valid without a unique title' do
+    product = Product.new(title:        products( :apple ).title,
+                          description:  'yyy',
+                          price:        1,
+                          image_url:    'fred.gif' )
+    assert product.valid?
+  end
+
 end
